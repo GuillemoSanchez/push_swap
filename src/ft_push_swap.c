64b77f262de134007d8027e50f0aa0043b40f5fb
guisanch <guisanch@student.42madrid.com>
2024-04-13 12:24:44 +0200

    añadido a .h

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -6,27 +6,11 @@
 /*   By: guisanch <guisanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 19:26:42 by guisanch          #+#    #+#             */
-/*   Updated: 2024/04/13 12:01:55 by guisanch         ###   ########.fr       */
+/*   Updated: 2024/04/13 12:24:38 by guisanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_pushswap.h"
-
-/*
-	sa-> intercambia 2 primeros elementos de a(Check si hay 1 numero o menos >:()
-	sb-> intercambia 2 primeros elementos de b(Check si hay 1 numero o menos >:()
-	ss-> los dos a la ves quiere decir que sa y sb se ejecutan
-	pa-> Toma el primer elemento del stack b y lo pone el primero del stack a
-		 (si b esta vacio no hace nada)
-	pb-> Toma el primer elemento del stack a y lo pone el primero del stack b
-		 (si a esta vacio no hace nada)
-	ra-> rota la lista a si tengo 1, 2, 3, 4 lo convierte en 4, 2, 3, 1
-	rb-> rota la lista b si tengo 1, 2, 3, 4 lo convierte en 4, 2, 3, 1
-	rr-> rota ambas listas
-	rra-> lo hace al contrario rota la lista a si tengo 4, 3, 2, 1 lo convierte en 1, 4, 3, 2
-	rrb-> lo hace al contrario rota la lista b si tengo 4, 3, 2, 1 lo convierte en 1, 4, 3, 2
-	rrr-> aplica los dos
-*/
 
 int	main(int argc, char *argv[])
 {
